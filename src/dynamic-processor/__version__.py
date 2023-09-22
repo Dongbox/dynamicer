ad7443591fe6b52edd6a88db8adee703fc366e9b
dongbox <sfreebobo@163.com>
2023-09-22 15:42:20 +0800

    Init directory at first time.

---
+++ src/dynamic-processor/__version__.py
@@ -0,0 +1,14 @@
+# .-. .-. .-. . . .-. .-. .-. .-.
+# |(  |-  |.| | | |-  `-.  |  `-.
+# ' ' `-' `-`.`-' `-' `-'  '  `-'
+
+__title__ = "dynamic-processor"
+__description__ = "Dynamically processor for python/C Lib."
+__url__ = "https://github.com/Dongbox/dynamic-profcessor"
+__version__ = "0.0.0"
+__build__ = 0x023100
+__author__ = "Dongbo Xie"
+__author_email__ = "dongbox@protonmail.com"
+__license__ = "Apache-2.0"
+__copyright__ = "Copyright Kenneth Reitz"
+__cake__ = "\u2728 \U0001f370 \u2728"
